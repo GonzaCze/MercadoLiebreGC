be35c56cf9e256ac52885a071a2fa69b8564b9e3
gonzacze <gonzalocze1@gmail.com>
2022-06-02 22:06:02 -0300

    Trabajo Mercado Liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const log = console.log;
+
+const express = require ("express");
+const app = express();
+const path = require('path');
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath));
+app.listen(3000, ()=>{
+    log('Servidor corriendo en el puerto 3000')
+});
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'views/home.html'))
+})
+app.get('/registro', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'views/registro.html'));
+})
+app.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'views/login.html'));
+})
+app.post("/datos", (req, res)=>{
+    res.send(req.body)
+})
